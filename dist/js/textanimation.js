e5b1f34209131affa245d61c6b7253b8b5a98178
DaffaMalik12 <dmalikakram@gmail.com>
2023-10-30 11:39:43 +0700

    Perubahan teks animasi

---
+++ dist/js/textanimation.js
@@ -0,0 +1,10 @@
+const typing = new Typed ("#typing",{
+    strings : ["Muhammad Daffa", "FrontEnd Developer"],
+    typeSpeed: 100,
+    backSpeed: 10,
+    backDelay: 500,
+    startDelay: 1000,
+    loop : true
+});
+
+
